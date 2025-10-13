7752be6711d85393c812b71b03605885105a53bc
carpbreadj <carpbreadj@gmail.com>
2025-10-13 21:35:51 +0900

    init: setup develop branch

---
+++ src/main/java/com/example/codegardener/GardenerWasApplication.java
@@ -0,0 +1,13 @@
+package com.example.codegardener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenerWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenerWasApplication.class, args);
+	}
+
+}
