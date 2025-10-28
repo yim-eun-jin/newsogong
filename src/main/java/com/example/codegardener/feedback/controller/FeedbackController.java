f34f7eee1b5394204700afb9af174145f8bfea8d
yimeunjin <yesim2j@yu.ac.kr>
2025-10-29 02:49:50 +0900

    new

--- src/main/java/com/example/codegardener/feedback/controller/FeedbackController.java
+++ src/main/java/com/example/codegardener/feedback/controller/FeedbackController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
